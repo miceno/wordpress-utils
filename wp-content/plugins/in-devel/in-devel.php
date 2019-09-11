4b7b543d2bb68ed3771ec1669c93aaca3a46212a
Orestes Sanchez <orestes@acm.org>
2019-09-11 20:18:47 +0200

    Update Google Analytics code.

--- wp-content/plugins/in-devel/in-devel.php
+++ wp-content/plugins/in-devel/in-devel.php
@@ -44,27 +44,31 @@ if ( ! class_exists( 'InDevelopment' ) ) {
 
 
 /*
-   google analytics.
+ * Google analytics.
+ *
+ * Add Google analytics code.
+ *
+ * Configure GA_USERID constant with the value for the Google Analytics tracking code.
+ *
+ * For example:
+ *
+ *  define('GA_USERID', 'XX-ZZZZZZ')
+ *
 */
-
 
 function custom_google_analytics() {
 	if ( defined( "GA_USERID" ) ) { ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo GA_USERID; ?>"></script>
         <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            window.dataLayer = window.dataLayer || [];
 
-            ga('create', '<?php echo GA_USERID; ?>', 'auto');
-            ga('send', 'pageview');
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+            gtag('config', '<?php echo GA_USERID; ?>');
         </script><?php
 	}
 }
